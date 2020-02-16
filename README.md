57e44e6455911111d74b119ed52329630cc0c314
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2020-02-16 17:26:59 +0100

    only 1 group

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # home-control
+
+`/opt/gpio-setup.sh`
+
+`/sys/class/gpio`
--- src/light-mapping.js
+++ src/light-mapping.js
@@ -1,45 +1,50 @@
-module.exports = [{
-    "from": {
-        "pin": 1,
-        "mode": "in",
-        "edge": "rising",
-        "button": null
-    },
-    "to": {
-        "type": "group",
-        "index": 6
-    }
-}, {
-    "from": {
-        "pin": 2,
-        "mode": "in",
-        "edge": "rising",
-        "button": null
-    },
-    "to": {
-        "type": "group",
-        "index": 6
-    }
-}, {
-    "from": {
-        "pin": 3,
-        "mode": "in",
-        "edge": "rising",
-        "button": null
-    },
-    "to": {
-        "type": "group",
-        "index": 6
-    }
-}, {
-    "from": {
-        "pin": 4,
-        "mode": "in",
-        "edge": "rising",
-        "button": null
-    },
-    "to": {
-        "type": "group",
-        "index": 6
-    }
-}]
+module.exports = [
+    {
+        "from": {
+            "pin": 1,
+            "mode": "in",
+            "edge": "rising",
+            "button": null
+        },
+        "to": {
+            "type": "group",
+            "index": 6
+        }
+    }, 
+    // {
+    //     "from": {
+    //         "pin": 2,
+    //         "mode": "in",
+    //         "edge": "rising",
+    //         "button": null
+    //     },
+    //     "to": {
+    //         "type": "group",
+    //         "index": 6
+    //     }
+    // }, 
+    // {
+    //     "from": {
+    //         "pin": 3,
+    //         "mode": "in",
+    //         "edge": "rising",
+    //         "button": null
+    //     },
+    //     "to": {
+    //         "type": "group",
+    //         "index": 6
+    //     }
+    // }, 
+    // {
+    //     "from": {
+    //         "pin": 4,
+    //         "mode": "in",
+    //         "edge": "rising",
+    //         "button": null
+    //     },
+    //     "to": {
+    //         "type": "group",
+    //         "index": 6
+    //     }
+    // }
+]
