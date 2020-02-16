93c383ebb59868b68ad175f89545d6a151d34298
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2020-02-16 14:52:45 +0100

    pin 1 to 4

--- src/light-mapping.js
+++ src/light-mapping.js
@@ -9,4 +9,37 @@ module.exports = [{
         "type": "group",
         "index": 6
     }
+}, {
+    "from": {
+        "pin": 2,
+        "mode": "in",
+        "edge": "rising",
+        "button": null
+    },
+    "to": {
+        "type": "group",
+        "index": 6
+    }
+}, {
+    "from": {
+        "pin": 3,
+        "mode": "in",
+        "edge": "rising",
+        "button": null
+    },
+    "to": {
+        "type": "group",
+        "index": 6
+    }
+}, {
+    "from": {
+        "pin": 4,
+        "mode": "in",
+        "edge": "rising",
+        "button": null
+    },
+    "to": {
+        "type": "group",
+        "index": 6
+    }
 }]
