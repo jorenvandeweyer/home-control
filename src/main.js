ffa0b8ffb1849681dd5e53c1c22e351fa19b22a8
Joren Vandeweyer <joren.vandeweyer@gmail.com>
2020-08-29 11:28:40 +0200

    log

--- src/main.js
+++ src/main.js
@@ -10,29 +10,24 @@ hue.on('ready', (bridge) => {
     console.log('ready');
 
     mapping.forEach(async conf => {
-        try {
-            const button = new Button(conf.from);
+        const button = new Button(conf.from);
 
-            button.on('rising', _ => console.log(conf.from.pin, Date.now(), 'rising'));
-            button.on('falling', _ => console.log(conf.from.pin, Date.now(), 'falling', ));
-            button.on('toggle', _ => console.log(conf.from.pin, Date.now(), 'toggle'));
-            button.on('double', _ => console.log(conf.from.pin, Date.now(), 'double'));
+        button.on('rising', _ => console.log(conf.from.pin, Date.now(), 'rising'));
+        button.on('falling', _ => console.log(conf.from.pin, Date.now(), 'falling', ));
+        button.on('toggle', _ => console.log(conf.from.pin, Date.now(), 'toggle'));
+        button.on('double', _ => console.log(conf.from.pin, Date.now(), 'double'));
 
-            if (!conf.to.index) return;
+        if (!conf.to.index) return;
 
-            const group = await bridge.Group.one(conf.to.index);
+        const group = await bridge.Group.one(conf.to.index);
 
-            button.on('toggle', _ => group.toggle());
-            button.on('double', _ => group.setState({
-                on: true,
-                bri: 254,
-                brightness: 254
-            }));
-            button.on('start', _ => group.dim());
-            button.on('stop', _ => group.freeze());
-        } catch (e) {
-            console.log('weird error',e)
-        }
+        button.on('toggle', _ => group.toggle());
+        button.on('double', _ => group.setState({
+            on: true,
+            bri: 254,
+        }));
+        button.on('start', _ => group.dim());
+        button.on('stop', _ => group.freeze());
     });
 });
 
