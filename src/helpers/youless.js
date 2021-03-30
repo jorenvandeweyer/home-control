2b7abe6fb3a892142063e8b742908568672ca6d4
Joren Vandeweyer <joren.vandeweyer@gmail.com>
2021-03-30 18:30:58 +0200

    test

--- src/helpers/youless.js
+++ src/helpers/youless.js
@@ -2,13 +2,55 @@ const axios = require('axios');
 
 class Youless {
   constructor () {
+    this.address = null
+    this.last = Date.now()
 
+    this.init()
+  }
+
+  async init () {
+    if (this.last > Date.now() - 60000) return
+
+    this.last = Date.now()
+
+    const instance = axios.default.create({
+      timeout: 2000,
+    })
+
+    const possibilities = Array.from(new Array(254).keys())
+
+    const promises = possibilities.map(async number => {
+      try {
+        const address = `http://192.168.1.${number + 1}/a?f=j`
+        await instance.get(address)
+        return address
+      } catch (e) {
+        return null
+      }
+    })
+
+    const results = await Promise.all(promises)
+
+    const youless = results.filter(addr => addr)
+
+    if (youless.length) {
+      this.address = youless[0]
+    }
   }
 
   async status () {
-    const res = await axios.get('http://192.168.1.157/a?f=j')
+    if (!this.address) {
+      this.init()
+    }
 
-    return res.data
+    try {
+      const res = await axios.get('http://192.168.1.156/a?f=j')
+
+      return res.data
+    } catch (e) {
+      this.address = null
+      this.init()
+    }
   }
 }
 
