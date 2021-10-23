d52ef52ca14e29ab52584d003724961ca7d36f12
Joren Vandeweyer <joren.vandeweyer@gmail.com>
2021-10-23 17:01:18 +0200

    clean

--- src/lighting/index.ts
+++ src/lighting/index.ts
@@ -26,6 +26,8 @@ hue.on('ready', (bridge: Bridge) => {
       button.on(event, () => console.log(conf.from.pin, Date.now(), event))
     })
 
+    if (!conf.to.items) return
+
     if (conf.to.type === 'group') {
       const group = await bridge.Group.one(conf.to.items.toString())
 
@@ -61,57 +63,6 @@ hue.on('ready', (bridge: Bridge) => {
         lights.forEach(light => light.freeze())
       })
     }
-  })
-
-  mapping.forEach(async conf => {
-    const button = new Button(conf.from);
-
-    ['rising', 'falling', 'toggle', 'double', 'start', 'stop'].forEach(event => {
-      button.on(event, () => console.log(conf.from.pin, Date.now(), event))
-    })
-
-    // if (!conf.to.items) return
-
-    // if (conf.to.type === 'group') {
-
-    // } else if (conf.to.type === 'light') {
-    //   const lights = await Promise.all(
-    //     conf.to.items.map(item => {
-    //       return bridge.Light.one(item)
-    //     })
-    //   )
-
-    //   button.on('double', _ => {
-    //     lights.forEach(light => light.setState({
-    //       on: true,
-    //       bri: 254
-    //     }))
-    //   })
-
-    //   button.on('toggle', _ => {
-    //     await Promise.all(lights.map(light => light.update()))
-
-    //     if (lights.some(light => light.state.on)) {
-    //       lights.forEach(light => light.off())
-    //     } else {
-    //       lights.forEach(light => light.on())
-    //     }
-    //   })
-
-    //   button.on('start', _ => {
-
-    //   })
-    // }
-
-    // const group = await bridge.Group.one(conf.to.items)
-
-    // button.on('toggle', _ => group.toggle())
-    // button.on('double', _ => group.setState({
-    //   on: true,
-    //   bri: 254
-    // }))
-    // button.on('start', _ => group.dim())
-    // button.on('stop', _ => group.freeze())
   })
 })
 
